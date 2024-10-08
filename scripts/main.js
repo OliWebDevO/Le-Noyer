3714a12b8c7c41d120b78d9bca7564545933a61e
OliWebDevO <oliver-vdb@hotmail.com>
2024-10-08 12:32:48 +0200

    header done, horaire tbf

--- scripts/main.js
+++ scripts/main.js
@@ -28,7 +28,7 @@ const swiper = new Swiper('.swiper', {
     direction: 'horizontal',
     loop: true,
     autoplay :  {
-      delay: 10000,
+      delay: 1000000,
       disableOnInteraction:false,
     },
   
